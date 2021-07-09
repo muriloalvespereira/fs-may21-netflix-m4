d8a93c418a5f2991b0aa94ba163f9d4546d1d559
Murilo Alves <murilo.udi@gmail.com>
2021-07-09 13:51:40 +0200

    Changed borders

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ function App() {
   return (
     <div className="App">
       <Container fluid>
-        <Row className="text-white mb-3 categories">Horror</Row>
+        <Row className="text-white mt-4 mb-3 categories">Horror</Row>
        <CarouselMovie category="horror"/>
-       <Row className="text-white mb-3 categories">Drama</Row>
+       <Row className="text-white mt-4 mb-3 categories">Drama</Row>
        <CarouselMovie category="drama"/>
-       <Row className="text-white mb-3 categories">Romance</Row>
+       <Row className="text-white mt-4 mb-3 categories">Romance</Row>
        <CarouselMovie category="romance"/>
+       <Row className="text-white mt-4 mb-3 categories">Comedy</Row>
+       <CarouselMovie category="comedy"/>
+       <Row className="text-white mt-4 mb-3 categories">Action</Row>
+       <CarouselMovie category="action"/>
       </Container>
     </div>
   );
